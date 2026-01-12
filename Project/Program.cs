2dd268752ea3574a867e533f7e34b62c9a212e8d
Virus903 <kirillwsac2000@gmail.com>
2026-01-12 17:26:23 +0300

    Add files via upload

---
+++ Project/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Battlegrounds HS Hub");
+Console.WriteLine("----------------");
+Console.WriteLine("1) База данных и админка");
+Console.WriteLine("2) Публичная часть - тир листы");
+Console.WriteLine("3) Конструктор сборок");
+Console.WriteLine("4) Детали и визуализация");
+Console.WriteLine("0) Выход");
+Console.WriteLine("----------------");
+Console.Write("Выберите пункт меню: ");
+
+var input = Console.ReadLine();
+Console.WriteLine($"Вы выбрали: {input}");
+Console.WriteLine("Пока это только каркас. Логика появится в следующих ЛР.");
